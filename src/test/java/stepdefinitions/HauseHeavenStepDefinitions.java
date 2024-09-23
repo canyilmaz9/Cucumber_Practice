684bd05ddd3f76a7a2c21d35928c93e3cbe1b9e0
Can Yilmaz <canyilmaz9999@gmail.com>
2024-09-23 12:12:58 +0200

    Hatasiz Commitv3

--- src/test/java/stepdefinitions/HauseHeavenStepDefinitions.java
+++ src/test/java/stepdefinitions/HauseHeavenStepDefinitions.java
@@ -40,7 +40,7 @@ public class HauseHeavenStepDefinitions {
         int totalResult = Integer.parseInt(totalResultinString);
 
         System.out.println("Total number of Result: " + totalResult);
-        //new commit 2
+        //new commit 3
 
     }
     @Then("Close page")
